f369e2b71ccac53993dc4e7d06339d19c0b18106
vaibhav7920 <vaibhav.parashar@protiviti.co.in>
2023-12-26 22:18:17 +0530

    commiting to use in personal pc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type:'server', name: 'TestServer', content: 'Just a test'}];
+  
   onServerAdded(serverData :{serverName:string, serverContent: string}) {
     this.serverElements.push({
       type: 'server',
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,11 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './cockpit.component.css'
 })
 export class CockpitComponent implements OnInit {
+  /* serverCreated and blueprintCreated  are event emitters  of generic type.
+      Output() decorator is used to make the other components listen
+  */
+   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();  // () are used to call the constructor of Event Emitter
+   @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   newServerName = '';
   newServerContent = '';
-  constructor(){
+  constructor() {
   }
 
-  ngOnInit(){}
-  
+  ngOnInit() { }
+
+  onAddServer() {
+    // This is what we emit on add server
+    this.serverCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+  }
+
+  onAddBlueprint(){
+    this.blueprintCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+  }
 }
